099621de8cfef223140f033ccb3d3498d41d2ef0
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2023-12-20 16:47:56 +0100

    först commit

---
+++ src/BeverageFactory.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+
+public class BeverageFactory {
+
+    protected Beverage beverage;
+
+    public Beverage getBeverage(String order) {
+        if (order.equalsIgnoreCase("darkroast"))
+            beverage = new DarkRoast();
+        else if (order.equalsIgnoreCase("darkroast+milk"))
+            beverage = new Milk(new DarkRoast());
+        else if (order.equalsIgnoreCase("darkroast+milk+mocha"))
+            beverage = new Mocha(new Milk(new DarkRoast()));
+        else if (order.equalsIgnoreCase("decaf"))
+            beverage = new Decaf();
+        else if (order.equalsIgnoreCase("decaf+milk"))
+            beverage = new Milk(new Decaf());
+        else if (order.equalsIgnoreCase("decaf+milk+mocha"))
+            beverage = new Milk(new Mocha(new Decaf()));
+        else {
+            System.out.println("Otillåtet argument för metoden getBeverage()");
+            throw new InputMismatchException();
+        }
+        return beverage;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 
     protected Beverage beverage;
+    protected BeverageFactory beverageFactory;
     public static void main(String[] args) {
         Main main = new Main();
-        main.beverage = new Milk(new Mocha(new DarkRoast()));
+        main.beverageFactory = new BeverageFactory();
+        main.beverage = main.beverageFactory.getBeverage("decaf+milk+mocha");
         System.out.println("En " + main.beverage.getDescription() + " som kostar " + main.beverage.cost() + " kr");
     }
 }
